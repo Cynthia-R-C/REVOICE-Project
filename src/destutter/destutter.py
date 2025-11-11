c691957f18fdd0148c105eb942d5cd2c6ab25d3a
Cynthia <119439581+Cynthia-R-C@users.noreply.github.com>
2025-11-10 21:45:57 -0600

    11/10/2025 Commit - StutterNet Retrained
    Retrained StutterNet with corrected MFCC global cmvn file.

---
+++ src/destutter/destutter.py
@@ -0,0 +1,53 @@
+# Basic, semi-pseudo code for destuttering
+# Cynthia Chen 11/9/2025
+
+import re
+
+# Top-most level
+def destutter(audio, txt, stutter_model):
+    '''Take in: original audio packet + transcribed txt of that packet from STT
+    Output: destuttered txt? Unsure how to deal with timestamps yet'''
+
+    # 1. Classify stuttering type
+    stutter_types = get_stutter_types(audio, stutter_model)  # returns a list of stutter types included, e.g. [/p, /r]
+
+    # 2. Remove stuttering from txt based on classified stutter types
+    if '/p' in stutter_types:
+        txt = remove_p_stutter(audio, txt)
+    if '/b' in stutter_types:
+        txt = remove_b_stutter(audio, txt)
+    if '/r' in stutter_types:
+        txt = remove_r_stutter(txt)
+    if '/wr' in stutter_types:
+        txt = remove_wr_stutter(txt)
+    if '/i' in stutter_types:
+        txt = remove_i_stutter(audio, txt)
+
+def remove_r_stutter(txt):
+    '''Remove repetitions of initial sounds in words from the transcribed text'''
+    # Simple approach: look for patterns like s-s-s-
+    # note special case - if a stuttered word is like 'b-b-bow-tie', we want to keep the full word 'bow-tie'
+    # Also include multi-letter starts, e.g. "I am st-st-st-stuttering" -> "I am stuttering"
+    # Works for repeating prefixes of up 1-3 letters
+
+    pattern = r'\b([a-zA-Z]{1,3})(?:-\1){1,}-([a-zA-Z]+)'
+    new = re.sub(pattern, r'\2', txt)
+    # This is pretty insane, I just found out you could do this, the logic is as such:
+    # \b ensures a word boundary
+    # ([a-zA-Z]{1,3}) captures the first one to three letters
+    # (?:-\1){1,} matches one or more repetitions of that previously identified group followed by -.
+        # ?: means a non-capturing group, so we don't store this part for back-referencing
+        # -\1 means “a dash followed by the same letter we captured before.” \1 refers back to the first (...) group.
+        # {1, } means “one or more times.”
+    # -([a-zA-Z]+) captures the rest of the real word (e.g., “ow” in “b-b-bow”).
+    # The replacement \2 reconstructs the proper word (“bow”) by just keeping the second group.
+    
+    return new
+
+
+# Testing
+if __name__ == "__main__":
+    test_txt = "I want a b-b-bow-tie a-a-and I st-st-stutter."
+    print("Original:", test_txt)
+    destuttered_txt = remove_r_stutter(test_txt)
+    print("Destuttered:", destuttered_txt)
